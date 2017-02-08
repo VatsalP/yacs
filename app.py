ab73f12970b6b5b6e0e064fef76f42921b5665c0
Vatsal Parekh <vatsalbparekh@gmail.com>
2017-02-08 15:42:45 +0530

    Added sqlite

--- app.py
+++ app.py
@@ -1,30 +1,39 @@
 """
-
+Ayy lmao
 """
 import time
 
 import bottle
 import valve.source.a2s
 
-app = bottle.Bottle()
-bottle.debug(True)
-SERVER_ADDRESS = ("yacs.cf", 27015)
+from bottle.ext import sqlite
+
+APP = bottle.Bottle()
+PLUGIN = sqlite.Plugin(dbfile='sourcemod.sq3')
+APP.install(PLUGIN)
+SERVER_ADDRESS = ("139.59.23.194", 27015)
 
 def a2s():
     """Retrieves info about the server
     """
-    server = valve.source.a2s.ServerQuerier(SERVER_ADDRESS)
-    t_send = time.time()
-    info = server.info()
-    ping = (time.time() - t_send) * 1000.0
-    players = server.players()
-    return [ping, info, players]
+    try:
+        server = valve.source.a2s.ServerQuerier(SERVER_ADDRESS)
+        t_send = time.time()
+        info = server.info()
+        ping = (time.time() - t_send) * 1000.0
+        #players = server.players()
+        return [True, ping, info] #, players]
+    except Exception:
+        return [False]
 
-
-@app.route('/')
-def index():
+@APP.route('/')
+def index(db):
     data = a2s()
-    return bottle.template("index", data=data)
+    ipinfo = db.execute(
+        "SELECT ccode, Count(*) as countrycode from ipinfo group by ccode order by countrycode desc"
+        ).fetchall()
+    return bottle.template("index", data=data, ipinfo=ipinfo)
 
 if __name__ == "__main__":
-    bottle.run(app, host='localhost', port=8080, reloader=True)
+    bottle.debug(True)
+    bottle.run(APP, host='localhost', port=8080, reloader=True)
